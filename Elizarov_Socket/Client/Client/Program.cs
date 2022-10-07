416002d3c081fa1484d2b3bb8b971fa98e33b31e
Andrian Elizarov <101263148+RussodOlera@users.noreply.github.com>
2022-10-07 12:49:16 +0200

    Aggiunta codice

---
+++ Elizarov_Socket/Client/Client/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SynchronusSocketClient
+{
+    public static void start()
+    {
+        byte[] bytes = new byte[1024];
+        try
+        {
+            IPAddress ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 5000);
+            Socket sender = new Socket(ipAddress.AddressFamily,SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                sender.Connect(remoteEP);
+
+                Console.WriteLine("Connesso con {0}",sender.RemoteEndPoint.ToString());
+                Console.WriteLine("Scegli\n" + "1. Sasso\n" + "2.Carta\n" + "3.Forbice\n");
+                var risp = Console.ReadLine();
+                byte[] msg = Encoding.ASCII.GetBytes("Sasso,Carta,Forbice<EOF>");
+                int bytesSent = sender.Send(msg);
+                int bytesRec = sender.Receive(bytes);
+                Console.WriteLine("L'avversario ha scelto {0}",Encoding.ASCII.GetString(bytes, 0, bytesRec));
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+                switch(bytes)
+                {
+                    case 1:
+
+                }
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+    public static int Main(String[] args)
+    {
+        start();
+        Console.WriteLine("Premere un tasto per continuare ");
+        Console.ReadLine();
+        return 0;
+    }
+}
---
+++ Elizarov_Socket/Server/Server/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+public class SynchronousSocketListener
+{
+    public static string data = null;
+
+    public static void start()
+    {
+        byte[] bytes = new Byte[1024];
+        IPAddress ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 5000);
+        Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+            while(true)
+            {
+                Console.WriteLine("In attesa di connessione...");
+                Socket handler = listener.Accept();
+                data = null;
+                while(true)
+                {
+                    int bytesRec = handler.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    if (data.IndexOf("<EOF>") > -1)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine("Messaggio del Client: ", data);
+                Random r = new Random();
+                var risp = r.Next(0, 3) + 1;
+                byte[] msg = Encoding.ASCII.GetBytes(risp.ToString());
+
+                handler.Send(msg);
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress ENTER to continue...");
+        Console.Read();
+    }
+    public static int Main(String[] args)
+    {
+        start();
+        return 0;
+    }
+}
